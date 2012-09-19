3e00725b88af00dba019c68638e74f66a8799e62
Alister Bulman <abulman@gmail.com>
2012-09-19 11:42:58 +0100

    adding to the header

--- public/index.php
+++ public/index.php
@@ -1,22 +1,29 @@
 <!doctype html>
 <html dir="ltr" lang="EN"> 
-<head> 
+<head>
 	<meta charset="UTF-8" /> 
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
 	<meta name="description" content="An initiative" /> 
-	<meta name="keywords" content="Jobs txt" /> 
-	<meta name="title" content="Jobs TXT: easily parsable " /> 
-	<title>JOBS TXT</title> 
-	<!--[if IE]> <script type="text/javascript" src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script> <![endif]--> 
-	<link type="text/plain" rel="author" href="http://jobstxt.org/humans.txt" />
-    <!-- PNG FIX for IE6 -->
-    <!-- http://24ways.org/2007/supersleight-transparent-png-in-ie6 -->
-    <!--[if lte IE 6]>
-        <script type="http://humanstxt.org/text/javascript" src="js/pngfix/supersleight-min.js"></script>
-    <![endif]-->
-    <!-- jQuery - the core -->
-    <!-- Sliding effect -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta name="author" content="Alister Bulman">
+  <!--link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/css/bootstrap-combined.min.css"-->
+  <link href="http://jobstxt.org/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+
+	<meta name="keywords" content="Jobs txt, simple, jobs" /> 
+	<meta name="title" content="Jobs TXT: easily parsable job listings and format" /> 
+	<title>Jobs TXT: easily parsable job listings file and format</title>
+
+        <!-- PNG FIX for IE6 -->
+        <!-- http://24ways.org/2007/supersleight-transparent-png-in-ie6 -->
+        <!--[if lte IE 6]>
+            <script type="http://humanstxt.org/text/javascript" src="js/pngfix/supersleight-min.js"></script>
+        <![endif]-->
+        <!-- jQuery - the core -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 </head>
+
 <body> 
 
 <header></header>
@@ -36,26 +43,31 @@
             <div id="content2">
                 <h1>About jobs.txt</h1>
 
-                <section class="intro">
-                    <article> 
-<blockquote>
-<a href="http://news.ycombinator.com/user?id=lifeisstillgood">lifeisstillgood</a> 17th Aug 2012 | <a href="http://news.ycombinator.com/item?id=4534059">link</a> | <a href="http://news.ycombinator.com/item?id=4532768">parent</a><br/>
-<p>We have robots.txt, humans.txt, business.txt</p>
+<section class="intro">
+<article> 
 
-<p>so whats wrong with jobs.txt</p>
+  <p>It all started with a post on the Ycombinator forum ...</p>
 
-<p>Parseable, searchable, uptodate (one assumes, and can easily be deprecated) and simple to throw up a cottage industry around.</p>
-</blockquote>
+  <blockquote>
+  <a href="http://news.ycombinator.com/user?id=lifeisstillgood">lifeisstillgood</a> 17th Aug 2012 | <a href="http://news.ycombinator.com/item?id=4534059">link</a> | <a href="http://news.ycombinator.com/item?id=4532768">parent</a><br/>
+  <p>We have robots.txt, humans.txt, business.txt</p>
 
-<h2>Thats a Good Idea!</h2>
-<p><a href="/jobs.txt">/jobs.txt</a></p>
-<hr>
-<p>If you would like to have your jobs.txt linked from this page, please send me an email - submissions@jobstxt.org.</p>
-<p>.... and an improved website is coming when I can - or, fork it, and let me know. <a href="https://github.com/alister/jobstxt.org">https://github.com/alister/jobstxt.org</a>.</p>
+  <p>so whats wrong with jobs.txt</p>
 
-                    </article>
-                </section>
-            </div> <!-- .content-->
+  <p>Parseable, searchable, uptodate (one assumes, and can easily be deprecated) and simple to throw up a cottage industry around.</p>
+  </blockquote>
+
+  <h2>Thats a Good Idea!</h2>
+  <p><a href="/jobs.txt">/jobs.txt</a></p>
+  <hr>
+  <p>If you would like to have your jobs.txt linked from this page, please send me an email - submissions@jobstxt.org.</p>
+  <p>.... and an improved website is coming when I can - or, fork it, and let me know. <a href="https://github.com/alister/jobstxt.org">https://github.com/alister/jobstxt.org</a>.</p>
+
+</article>
+</section>
+</div> <!-- .content-->
+
+<!-- ======================================================== -->
 
 <footer>
     <section>
@@ -80,8 +92,8 @@ License</a></p>
 <p>Website by Alister Bulman <a href="http://abulman.co.uk">http://abulman.co.uk</a> | 
 	<a href="http://phpscaling.com">http://phpscaling.com</a> | 
 	<a href="http://careers.stackoverflow.com/alister">http://careers.stackoverflow.com/alister</a></p>
-<p>Original idea from a post by 'lifeisstillgood', <a href="http://news.ycombinator.com/item?id=4534059">http://news.ycombinator.com/item?id=4534059</a>
-Base HTML blatently stolen from <a href="http://humanstxt.org/">http://humanstxt.org/</a> and then cruelly torn to shreds</p>
+<p>Original idea from a post by 'lifeisstillgood', on news.ycombinator.com.
+  Base HTML blatently stolen from <a href="http://humanstxt.org/">http://humanstxt.org/</a> and then cruelly torn to shreds</p>
 
 
 		</div> <!-- .wrapper-->
